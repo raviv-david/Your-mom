932abcbba75aa303eed599ce6288db2abbf7b842
David Raviv <raviv8david@gmail.com>
2024-06-27 22:49:24 +0300

    minor changes

--- moses/scripts/playerMovement.gd
+++ moses/scripts/playerMovement.gd
@@ -1,37 +1,41 @@
 extends CharacterBody2D
 
-
-
-const SPEED = 220.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 13
+const MAX_SPEED = 130
+const JUMP_VELOCITY = -193
+const MAX_FALLING_SPEED = 700
+const GRAVITY = 450
 
 var jumps = 1
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
 func _physics_process(delta):
-	# Add the gravity.
+	# Apply gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += GRAVITY * delta
+		velocity.y = clamp(velocity.y, -99999, 175)
 	
-	if is_on_floor():
-		jumps = 1
-		
-	# Handle jump.
+	if is_on_floor(): 
+		jumps = 2
+	
+	# Handle jump
 	if Input.is_action_just_pressed("moveJump") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
 		jumps -= 1
 		
-	
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement/deceleration
 	var direction = Input.get_axis("moveLeft", "moveRight")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED/13)
-
+	if direction != 0:
+		if !is_on_floor():
+			velocity.x += (direction * SPEED) / 2.5
+		else:
+			velocity.x += direction * SPEED
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+		
+		# Adjust character sprite direction
+		if $Sprite2D:
+			$Sprite2D.flip_h = direction < 0
+	else: # Deceleration
+		velocity.x = move_toward(velocity.x, 0, SPEED/2)
+	
+	# Apply movement
 	move_and_slide()
